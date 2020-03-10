a238315cd12bd52831ff855b1cb06d47b3fc7e97
Keelana Char <keelanachar@gmail.com>
2020-03-09 21:09:14 -0500

    Add functions to compare user choice vs comp choice

--- app.js
+++ app.js
@@ -7,3 +7,47 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      console.log("USER WINS!");
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      console.log("USER LOSES.");
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      console.log("DRAW");
+      break;
+  }
+
+};
+
+function main () {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  });
+
+  paper_div.addEventListener('click', function() {
+    game("p");
+  });
+
+  scissors_div.addEventListener('click', function() {
+    game("s");
+  });
+};
+
+main();
