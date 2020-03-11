c5e533539191a2aed71b1afee1ff8e67589d5eb1
Keelana Char <keelanachar@gmail.com>
2020-03-10 22:51:49 -0500

    Add convert to word and win functions

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
+let userScore = 0;
+let computerScore = 0;
+let userScore_span = document.getElementById("user-score");
+let computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+let result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,25 +14,47 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors";
+};
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`;
+};
+
+
+function lose() {
+  console.log("lose");
+};
+
+function draw() {
+  console.log("draw");
+};
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
     case "rs":
     case "pr":
     case "sp":
-      console.log("USER WINS!");
+      win(userChoice, computerChoice);
       break;
     case "rp":
     case "ps":
     case "sr":
-      console.log("USER LOSES.");
+      lose(userChoice, computerChoice);
       break;
     case "rr":
     case "pp":
     case "ss":
-      console.log("DRAW");
+      draw(userChoice, computerChoice);
       break;
-  }
+  };
 
 };
 
